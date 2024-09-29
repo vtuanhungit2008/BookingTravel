5aeff1194f09f43f764234912f0be129b218c814
Vo Tuan Hung <vtuanhungit2008@gmail.com>
2024-09-29 22:37:52 +0700

    connect db using fisma and supabase, using cleak auth

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -13,9 +13,20 @@ import { links } from '@/utils/links';
 import SignOutLink from './SignOutLink';
 import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs/server';
+
+
 function LinksDropdown() {
   const { userId } = auth();
+  console.log("userid :",userId);
+  
   const isAdminUser = userId === process.env.ADMIN_USER_ID;
+  if(isAdminUser){
+   console.log("welcome Admin");
+   
+  }else{
+    console.log("deo phai");
+    
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -42,7 +53,9 @@ function LinksDropdown() {
           {links.map((link) => {
             if (link.label === 'admin' && !isAdminUser) return null;
             return (
+        
               <DropdownMenuItem key={link.href}>
+                     
                 <Link href={link.href} className='capitalize w-full'>
                   {link.label}
                 </Link>
@@ -51,9 +64,13 @@ function LinksDropdown() {
           })}
           <DropdownMenuSeparator />
           <DropdownMenuItem>
+            
             <SignOutLink />
           </DropdownMenuItem>
+          
         </SignedIn>
+         
+        
       </DropdownMenuContent>
     </DropdownMenu>
   );
---
+++ utils/action.ts
@@ -0,0 +1,58 @@
+'use server';
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { profileSchema } from './schemas';
+
+export const createProfileAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error('Please login to create a profile');
+
+    const rawData = Object.fromEntries(formData);
+    console.log("rawdata",rawData);
+
+    
+    const validatedFields = profileSchema.parse(rawData);
+console.log("valider",validatedFields);
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
+  }
+  redirect('/');
+};
+
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
+};
